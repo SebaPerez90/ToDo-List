9222947f6c4fb6eab3ab2d778dfc67e943ebfeaf
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2024-01-23 16:49:34 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,59 @@
-import TaskForm from './components/TaskForm'
-import TasksList from './components/TaskList';
-import React, { createContext, useState } from 'react';
+import TaskForm from "./components/TaskForm";
+import TasksList from "./components/TaskList";
+import { createContext, useState } from "react";
 
-export const AppContext = createContext(null)
+export const AppContext = createContext(null);
 
 function App() {
-
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
   const [newTask, setNewTask] = useState([]);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if (input.trim() == '') {
-      let errMjs = document.querySelector('.errorMsj-container')
+    if (input.trim() == "") {
+      let errMjs = document.querySelector(".errorMsj-container");
 
-      errMjs.innerHTML +=
-        `<p class="errorMsj text-2xl text-center bg-slate-800 text-white p-5 px-10 opacity-50 rounded-md mt-6 z-50 font-serif shadow-gray-600 shadow-xl"
+      errMjs.innerHTML += `<p class="errorMsj text-2xl text-center bg-slate-800 text-white p-5 px-10 opacity-50 rounded-md mt-6 z-50 font-serif shadow-gray-600 shadow-xl"
         >You must add at least one task plis</p>`;
 
       setTimeout(() => {
-        document.querySelector('.errorMsj').remove()
+        document.querySelector(".errorMsj").remove();
       }, 1500);
-
     } else {
+      setNewTask([
+        ...newTask,
+        {
+          id: newTask.length,
+          task: input,
+          completed: false,
+        },
+      ]);
 
-      setNewTask([...newTask, {
-        id: newTask.length,
-        task: input,
-        completed: false,
-      }])
-
-      setInput('')
-      document.querySelector('.formSection-input').value = ''
+      setInput("");
+      document.querySelector(".formSection-input").value = "";
     }
   };
 
-
   return (
-    <main className='bg-gray-300 h-screen box-border pepito'>
-      <AppContext.Provider value={{ handleSubmit, input, setInput, newTask, setNewTask }} >
+    <main className="bg-gray-300 h-screen box-border pepito">
+      <AppContext.Provider
+        value={{ handleSubmit, input, setInput, newTask, setNewTask }}
+      >
         <TaskForm />
         <TasksList />
       </AppContext.Provider>
       <a
-        className='absolute bottom-3 font-bold text-gray-500 drop-shadow-sm right-5 hover:text-violet-600 ease-out text-lg'
-        href='https://github.com/SebaPerez90'
-        target='_blank'>Sebastian Perez © 2023 | do it with 💜</a>
+        rel="noreferrer"
+        className="absolute bottom-3 font-bold text-gray-500 drop-shadow-sm right-5 hover:text-violet-600 ease-out text-lg"
+        href="https://github.com/SebaPerez90"
+        target="_blank"
+      >
+        Sebastian Perez © 2023 | do it with 💜
+      </a>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,28 +1,34 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../App';
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 const TaskForm = () => {
+  const { handleSubmit, setInput } = useContext(AppContext);
 
-	const { handleSubmit, setInput } = useContext(AppContext)
-
-	return (
-		<section>
-			<h1 className='text-center font-bold text-6xl text-white pt-5 drop-shadow-md font-serif'>To Do List </h1>
-			<form
-				className='flex justify-center content-center mt-14 '
-				onSubmit={handleSubmit}>
-				<input
-					className='formSection-input text-xl outline-none p-3 drop-shadow-xl placeholder:italic rounded-l-md sm:w-80
-					pl-5 text-neutral-500 italic font-medium focus:border-green-700 focus:border-2'
-					autoFocus
-					placeholder='add a new task . . .'
-					onChange={e => setInput((e.target.value))}
-					type='text' />
-				<button className='bg-green-700 text-white px-4 pb-1 drop-shadow-xl rounded-r-md font-black text-3xl hover:bg-green-400 ease-out'>+</button>
-			</form>
-			<div className='errorMsj-container flex justify-center content-center flex-col flex-wrap'></div>
-		</section>
-	)
-}
+  return (
+    <section>
+      <h1 className="text-center font-bold text-6xl text-white pt-5 drop-shadow-md font-serif">
+        To Do List{" "}
+      </h1>
+      <form
+        className="flex justify-center content-center mt-14 "
+        onSubmit={handleSubmit}
+      >
+        <input
+          id="formSection-input"
+          className="formSection-input text-xl outline-none p-3 drop-shadow-xl placeholder:italic rounded-l-md sm:w-80
+					pl-5 text-neutral-500 italic font-medium focus:border-green-700 focus:border-2"
+          autoFocus
+          placeholder="add a new task . . ."
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+        />
+        <button className="bg-green-700 text-white px-4 pb-1 drop-shadow-xl rounded-r-md font-black text-3xl hover:bg-green-400 ease-out">
+          +
+        </button>
+      </form>
+      <div className="errorMsj-container flex justify-center content-center flex-col flex-wrap"></div>
+    </section>
+  );
+};
 
 export default TaskForm;
